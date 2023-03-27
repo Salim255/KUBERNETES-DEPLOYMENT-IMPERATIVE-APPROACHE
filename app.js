7d769e47b063568825f27550fc9103fbb94a281c
Salim <salim.hassan.welleat@gmail.com>
2023-03-28 01:37:35 +0200

    Add: imagepullpolicy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
-    <p>Hello Salim, how are you doing</p>
+    <p>Hello Salim 🌈</p>
   `);
 });
 
